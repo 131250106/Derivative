4de1c376efc5e27c7fe0446033c154c81c564102
John <John@172.25.132.148>
2015-08-13 16:56:34 +0800

    更新了User类，增加了用户的订单属性

--- src/main/java/data/User.java
+++ src/main/java/data/User.java
@@ -1,5 +1,7 @@
 package data;
 
+import java.util.ArrayList;
+
 /**
  * Created by Nifury on 8/13/2015.
  */
@@ -8,27 +10,59 @@ public class User {
 	private String name;			//用户姓名
 	private String user;			//用户账号
 	private String password; 	//用户密码
+	
+	private ArrayList<Order> listofOrders;			//用户所持有的所有订单
 
-	public User(String id, String name, String user, String password) {
+	
+	public User(String id, String name, String user, String password,
+			ArrayList<Order> listofOrders) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.user = user;
 		this.password = password;
+		this.listofOrders = listofOrders;
 	}
 
 	public String getId() {
 		return id;
 	}
 
+	public void setId(String id) {
+		this.id = id;
+	}
+
 	public String getName() {
 		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public String getUser() {
 		return user;
 	}
 
+	public void setUser(String user) {
+		this.user = user;
+	}
+
 	public String getPassword() {
 		return password;
 	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public ArrayList<Order> getListofOrders() {
+		return listofOrders;
+	}
+
+	public void setListofOrders(ArrayList<Order> listofOrders) {
+		this.listofOrders = listofOrders;
+	}
+
+
 }
