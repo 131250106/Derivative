325954eea3cf1269aa26c90ffb21ecf993fdfc97
Nifury <yusheng628@126.com>
2015-08-13 16:35:32 +0800

    implement service

--- src/main/java/bl/ServiceImpl.java
+++ src/main/java/bl/ServiceImpl.java
@@ -1,10 +1,37 @@
 package bl;
 
 import blservice.Service;
+import data.User;
+
+import java.rmi.RemoteException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * Created by Nifury on 8/13/2015.
  */
 public class ServiceImpl implements Service {
-	
+
+    private Map<String, User> userData = new HashMap<>();
+
+    @Override
+    public boolean login(String username, String password) throws RemoteException {
+        User user = userData.get(username);
+        return user != null && user.getPassword().equals(password);
+    }
+
+    @Override
+    public String register(String username, String password, String name) {
+        User user = userData.get(username);
+        if (user != null)
+            return "Username already exists.";
+        userData.put(username, new User(UUID.randomUUID().toString(), name, username, password));
+        return "success";
+    }
+
+    @Override
+    public void placeholder() throws RemoteException {
+
+    }
 }
