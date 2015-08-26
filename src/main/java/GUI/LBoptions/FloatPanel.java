0e44eefa2563cc9bf8c6ffd7afa197066ba4f532
yuqiGao <574057633@qq.com>
2015-08-26 19:37:57 +0800

    add default selected jratidbutton

--- src/main/java/GUI/LBoptions/FloatPanel.java
+++ src/main/java/GUI/LBoptions/FloatPanel.java
@@ -155,6 +155,10 @@ public class FloatPanel extends LBoptionsPanel{
 		this.add(Europe);
 		this.add(America);
 		
+		America.setSelected(true);
+		LookDown.setSelected(true);
+		
+		
 		executePriceLabel = new JLabel("执行价格:");
 		executePriceLabel.setFont(font);
 		executePriceLabel.setBounds(213+100, 260+50, LABEL_WIDTH, LABEL_HEIGHT);
